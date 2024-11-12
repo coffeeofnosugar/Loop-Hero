c71dea78118f7dddb0eb0d386f25e3207ebac8bc
coffee <985942825@qq.com>
2024-11-13 00:29:10 +0800

    Lua Hello World

--- Assets/Game/Scripts/GroundGenerator.cs
+++ Assets/Game/Scripts/GroundGenerator.cs
@@ -1,9 +1,14 @@
+using System;
+using Coffee.Management;
 using UnityEngine;
 
 namespace Coffee
 {
     public class GroundGenerator : MonoBehaviour
     {
-        
+        private void Awake()
+        {
+            LuaManager.Instance.DoLuaFile("Assets/Game/LuaScripts/HelloWorld.lua.txt");
+        }
     }
 }
---
+++ Assets/Game/Scripts/Management/LuaManager.cs
@@ -0,0 +1,55 @@
+﻿using Tools;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using XLua;
+
+namespace Coffee.Management
+{
+    public class LuaManager : PersistentHumbleSingleton<LuaManager>
+    {
+        private LuaEnv luaEnv;
+
+        public LuaTable Global => luaEnv.Global;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            if (luaEnv != null) return;
+            luaEnv = new LuaEnv();
+            luaEnv.AddLoader(CustomLoader);
+        }
+        
+        private byte[] CustomLoader(ref string filepath)
+        {
+            AsyncOperationHandle<TextAsset> handle = Addressables.LoadAssetAsync<TextAsset>(filepath);
+            handle.WaitForCompletion();
+            return handle.Status == AsyncOperationStatus.Succeeded ? handle.Result.bytes : null;
+        }
+        
+        public void DoLuaFile(string fileName)
+        {
+            string str = $"require('{fileName}')";
+            DoString(str);
+        }
+
+        public void DoString(string str)
+        {
+            if (luaEnv == null) return;
+            luaEnv.DoString(str);
+        }
+
+        public void Tick()
+        {
+            if (luaEnv == null) return;
+            luaEnv.Tick();
+        }
+
+        public void OnDestroy()
+        {
+            if (luaEnv == null) return;
+            luaEnv.Dispose();
+            luaEnv = null;
+        }
+    }
+}
