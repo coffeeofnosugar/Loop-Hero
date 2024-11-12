379afd2b91017f51c9773e0c521d262858e9f2c6
coffee <985942825@qq.com>
2024-11-13 00:56:00 +0800

    Main.lua文件

--- Assets/Game/Scripts/GroundGenerator.cs
+++ Assets/Game/Scripts/GroundGenerator.cs
@@ -1,14 +1,24 @@
 using System;
 using Coffee.Management;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace Coffee
 {
     public class GroundGenerator : MonoBehaviour
     {
+        public GameObject groundPrefab;
+        
         private void Awake()
         {
-            LuaManager.Instance.DoLuaFile("Assets/Game/LuaScripts/HelloWorld.lua.txt");
+            LuaManager.Instance.DoLuaFile("Assets/Game/LuaScripts/Main.lua.txt");
+            // groundPrefab = Addressables.LoadAssetAsync<GameObject>("Assets/Game/Prefabs/Ground.prefab").Result;
+            // Debug.Log(groundPrefab);
+        }
+
+        private void Update()
+        {
+            // Debug.Log(groundPrefab);
         }
     }
 }
---
+++ Assets/Game/Scripts/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Coffee.Management;
+using UnityEngine;
+
+namespace Coffee
+{
+    public class Test : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var a = LuaManager.Instance.Global.Get<float>("a");
+            Debug.Log(a);
+        }
+    }
+}
